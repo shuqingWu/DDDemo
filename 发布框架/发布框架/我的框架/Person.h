ca6c62d9b173ae39e4163ee56f9021385f3b1a4c
shuqingWu <m13058357821@163.com>
2018-03-25 13:13:02 +0800

    框架开发完成

---
+++ 发布框架/发布框架/我的框架/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  发布框架
+//
+//  Created by 树清吴 on 2018/3/25.
+//  Copyright © 2018年 DD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+- (void)eat;
+
+@end
---
+++ 发布框架/发布框架/我的框架/Person.m
@@ -0,0 +1,17 @@
+//
+//  Person.m
+//  发布框架
+//
+//  Created by 树清吴 on 2018/3/25.
+//  Copyright © 2018年 DD. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+- (void)eat {
+    NSLog(@"%@ eat", self.name);
+}
+
+@end
